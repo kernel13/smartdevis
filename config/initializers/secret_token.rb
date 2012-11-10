cca408e3d36da6f9b2b71861c05784c4cbe7b010
busyboard <contact@bb.c-coool.com>
2012-11-10 19:29:00 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smartdevis::Application.config.secret_token = '52555575616cc2baa417526baa81291e29a746aaa657dbfd5f8968c59ecae08aa73700253e113796eb4b0d9fd07e22697b4a62d4d8f3b920d2294945296a4c1a'
